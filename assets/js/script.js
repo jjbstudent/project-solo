8480c20a911c0f81b95f63fb62c43bd22ed726a1
jermaine <jblearn2023@gmail.com>
2024-02-15 13:30:13 +0000

    styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 console.log('test')
+
+
+
+var username = "jermaine";
+var email = "j.bandoo@gmail.com"; 
+var password = "123456";
+
+if (username && password) { 
+  console.log("Sign in successful");
+} else {
+  console.log("Try again");  
+}
+
