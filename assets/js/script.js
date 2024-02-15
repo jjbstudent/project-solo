65ef042d5cf19d28e9b492884167c78b6f51ea57
jermaine <jblearn2023@gmail.com>
2024-02-15 15:01:17 +0000

    form properties

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,36 @@
 console.log('test')
 
+let signupBtn = document.getElementById('signupBtn');
+let signinBtn = document.getElementById('signinBtn');
+let nameField = document.getElementById('nameField');
+let title = document.getElementById('title');
 
 
-var username = "jermaine";
-var email = "j.bandoo@gmail.com"; 
-var password = "123456";
 
-if (username && password) { 
-  console.log("Sign in successful");
-} else {
-  console.log("Try again");  
+//when the sign in button is clicked it will change the following HTML elements
+signinBtn.onclick = function() {  
+    nameField.style.maxHeight = "0";
+    title.innerHTML = 'Sign In';
+    signupBtn.classList.add('disable');
+    signinBtn.classList.remove('disable');
 }
+signupBtn.onclick = function() {  
+    nameField.style.maxHeight = "60px";
+    title.innerHTML = 'Sign Up';
+    signupBtn.classList.remove('disable');
+    signinBtn.classList.add('disable');
+}
+
+
+
+
+// var username = "jermaine";
+// var email = "j.bandoo@gmail.com"; 
+// var password = "123456";
+
+// if (username && password) { 
+//   console.log("Sign in successful");
+// } else {
+//   console.log("Try again");  
+// }
 
---
+++ assets/ref/pseudocode.md
@@ -0,0 +1,17 @@
+User Story
+user will sign up at start
+store user input details email, password, username
+save to local storage to be accessed later to confirm correct sign in details
+once confirmed go to new HTML page
+Movie page on load up will load all featured movies and tv shows from a movie API
+on hover display description of movies 
+filter options displayed in side bare or modal
+filter options include new movies & tv shows, genre, by rating etc
+user will be able add items to their watch list
+
+capture sign up data
+Add a <button type="submit"> to trigger form submit
+Listen for the 'submit' event
+Prevent default behavior
+Validate and process the form data
+Make server request to save user details
