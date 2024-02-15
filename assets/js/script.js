6af64a92f48fea7d95be888d0afd5150464168f8
jermaine <jblearn2023@gmail.com>
2024-02-15 16:48:14 +0000

    store user data

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,40 @@ let title = document.getElementById('title');
 
 
 
-//when the sign in button is clicked it will change the following HTML elements
-signinBtn.onclick = function() {  
+//when the signin botton i pressed it will change elements and validate user input
+signinBtn.addEventListener('click', function() {  
     nameField.style.maxHeight = "0";
     title.innerHTML = 'Sign In';
     signupBtn.classList.add('disable');
     signinBtn.classList.remove('disable');
-}
-signupBtn.onclick = function() {  
+});
+
+// sign up button will change elements and store user input 
+signupBtn.addEventListener('click', function() {  
     nameField.style.maxHeight = "60px";
     title.innerHTML = 'Sign Up';
     signupBtn.classList.remove('disable');
     signinBtn.classList.add('disable');
-}
 
+    // Get the values from the input fields
+    const userName = document.getElementById('userName').value;
+    const userEmail = document.getElementById('userEmail').value;
+    const userPassword = document.getElementById('userPassword').value;
+
+    // Added so i can check if local storage is supported by the browser
+    if (typeof(Storage) !== "undefined") {
+        // Store the values in local storage for sign-up
+        localStorage.setItem('userName', userName);
+        localStorage.setItem('userEmail', userEmail);
+        localStorage.setItem('userPassword', userPassword);
+
+        // You can add any other logic or redirect the user as needed
+        console.log('Sign-up data has been stored in local storage.');
+    } else {
+        // If local storage is not supported, return error message
+        console.log('Local storage is not supported by your browser.');
+    }
+});
 
 
 
